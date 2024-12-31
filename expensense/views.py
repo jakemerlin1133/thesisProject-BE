b7ab0f505872e0d2e3d8140e8b4d4ce4f8fd1fe1
jakemerlin <jakemerlin123@gmail.com>
2024-12-31 14:42:36 +0800

    changes

--- expensense/views.py
+++ expensense/views.py
@@ -412,7 +412,7 @@ def login(request):
             "user_name": user.user_name,
             "email": user.email,
             "first_name": user.first_name,
-            "last_name": user.last_name
+            "last_name": user.last_names
         }
     }, status=status.HTTP_200_OK)
     
