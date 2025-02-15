6d1acb744c6109039f0959362606c88c54fe41cd
jakemerlin1133 <114642760+jakemerlin1133@users.noreply.github.com>
2025-02-15 14:48:58 +0800

    fixed

--- thesisProject/wsgi.py
+++ thesisProject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "expensense.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thesisProject.settings")
 
 application = get_wsgi_application()
