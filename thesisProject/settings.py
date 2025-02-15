5ca2b7771f54520b734ebce2a6ca14f508afaeb5
jakemerlin1133 <114642760+jakemerlin1133@users.noreply.github.com>
2025-02-15 22:29:23 +0800

    preparing

--- thesisProject/settings.py
+++ thesisProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,8 +106,9 @@ WSGI_APPLICATION = "thesisProject.wsgi.application"
 #     }
 # }
 
+load_dotenv()
 DATABASES = {
-    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    "default": dj_database_url.config(default="sqlite:///db.sqlite3")
 }
 
 
