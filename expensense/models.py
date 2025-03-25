3d8352ddb4ec6cb8add08563ee56c8bab4084ac9
jakemerlin1133 <jakemerlin123@gmail.com>
2025-03-25 11:25:53 +0800

    update database

--- expensense/models.py
+++ expensense/models.py
@@ -30,7 +30,7 @@ class Category(models.Model):
         return self.category
      
 class Store(models.Model):
-    store = models.CharField(max_length=20, unique=True)
+    store = models.CharField(max_length=255, unique=True)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
            
 class Expense(models.Model):
